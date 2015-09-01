5eb10f41d4bacb165b937c4cb0726608e4bfc1f0
Michelle <michparlan@yahoo.com>
2015-09-01 10:40:24 +0800

    Initial Commit

---
+++ UmbrellaCorp-HW03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UmbrellaCorp-HW03
+//
+//  Created by Michelle Ann Parlan on 9/1/15.
+//  Copyright (c) 2015 Michelle Ann Parlan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
